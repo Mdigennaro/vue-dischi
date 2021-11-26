e0a5be5ea6fae30a1d72b802b79291bf0761f1f4
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-26 13:13:10 +0100

    first-commit + header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="logo d-flex align-items-center">
+    <img src="../assets/img/logo-spotify.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  nome: "Header"
+}
+</script>
+
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap.scss';
+
+  .logo{
+    width: 100%;
+    height: 60px;
+    background-color: #2e3a46;
+
+    img{
+      width: 70px;
+    }
+  }
+
+</style>
